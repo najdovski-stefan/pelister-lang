6ebea791252d5a62aaee5195e8b849c38ce16ded
Stefan Najdovski <stefan.najdovski@aol.com>
2022-07-14 13:40:05 +0200

    README.md

--- test/parser_test.cpp
+++ test/parser_test.cpp
@@ -1,19 +1,27 @@
 #include <gtest/gtest.h>
 #include "lexer.hpp"
 #include "parser.hpp"
-
-TEST(ParserTest, CorrectlyTokenizesInput) {
+#include "ast.hpp"
+TEST(ParserTest, ConstructsCorrectAstForSimpleExpression) {
     std::string input = "5 - 2";
     Lexer lexer(input);
     Parser parser(lexer);
 
-    parser.parse();
+    std::unique_ptr<AstNode> ast_root = parser.parse();
+    ASSERT_NE(ast_root, nullptr);
 
-    const auto& tokens = parser.getTokens();
-    ASSERT_EQ(tokens.size(), 3);
-    EXPECT_EQ(tokens[0].type, TokenType::Number);
-    EXPECT_EQ(tokens[0].text, "5");
-    EXPECT_EQ(tokens[1].type, TokenType::Minus);
-    EXPECT_EQ(tokens[2].type, TokenType::Number);
-    EXPECT_EQ(tokens[2].text, "2");
+    auto* root_node = dynamic_cast<BinaryOpNode*>(ast_root.get());
+    ASSERT_NE(root_node, nullptr) << "Root node should be a BinaryOpNode for '5 - 2'";
+
+    EXPECT_EQ(root_node->toString(), "-");
+
+    const auto& left_child = root_node->getLeft();
+    auto* left_num = dynamic_cast<const NumberNode*>(&left_child);
+    ASSERT_NE(left_num, nullptr) << "Left child should be a NumberNode";
+    EXPECT_EQ(left_num->getValue(), 5.0);
+
+    const auto& right_child = root_node->getRight();
+    auto* right_num = dynamic_cast<const NumberNode*>(&right_child);
+    ASSERT_NE(right_num, nullptr) << "Right child should be a NumberNode";
+    EXPECT_EQ(right_num->getValue(), 2.0);
 }
