2886373e7c431232bc533a5088775d235dae193d
Stefan Najdovski <stefan.najdovski@aol.com>
2022-07-16 18:30:22 +0200

    feat: load from file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -2,20 +2,13 @@
 #include <string>
 #include <vector>
 #include <filesystem>
+#include <fstream>
+#include <sstream>
 
 #include "lexer.hpp"
 #include "parser.hpp"
 #include "AstVisualizer.hpp"
 #include "Interpreter.hpp"
-
-bool find_option(int argc, char* argv[], const std::string& option) {
-    for (int i = 1; i < argc; ++i) {
-        if (std::string(argv[i]) == option) {
-            return true;
-        }
-    }
-    return false;
-}
 
 void runRepl() {
     Interpreter interpreter;
@@ -27,10 +20,7 @@ void runRepl() {
         if (!std::getline(std::cin, line) || line == "bye") {
             break;
         }
-
-        if (line.empty()) {
-            continue;
-        }
+        if (line.empty()) continue;
 
         try {
             Lexer lexer(line);
@@ -44,44 +34,52 @@ void runRepl() {
     }
 }
 
-void runSingle(const std::string& input, bool generate_ast, const std::string& exe_path_str) {
-    Lexer lexer(input);
-    Parser parser(lexer);
-    Interpreter interpreter;
+void runFile(const std::string& filepath) {
+    std::ifstream file(filepath);
+    if (!file.is_open()) {
+        std::cerr << "Error: Could not open file '" << filepath << "'" << std::endl;
+        return;
+    }
+
+    std::stringstream buffer;
+    buffer << file.rdbuf();
+    std::string source_code = buffer.str();
 
     try {
-        auto ast_root = parser.parse();
-
-        if (generate_ast) {
-            std::cout << "AST generation flag detected." << std::endl;
-            std::filesystem::path exe_path(exe_path_str);
-            std::filesystem::path output_path = exe_path.parent_path() / "ast.dot";
-
-            AstVisualizer visualizer;
-            visualizer.generateDot(*ast_root, output_path.string());
-
-            std::cout << "\nTo generate an image, run:" << std::endl;
-            std::cout << "dot -Tpng " << output_path.string() << " -o ast.png" << std::endl;
-            return;
-        }
-
-        interpreter.evaluate(*ast_root);
-        std::cout << "Final stack state:" << std::endl;
+        Lexer lexer(source_code);
+        Parser parser(lexer);
+        Interpreter interpreter;
+        auto ast = parser.parse();
+        interpreter.evaluate(*ast);
+        std::cout << "Program finished. Final stack state:" << std::endl;
         interpreter.printStack();
-
     } catch (const std::exception& e) {
         std::cerr << "Error: " << e.what() << std::endl;
     }
 }
 
+void printUsage(const char* program_name) {
+    std::cout << "Usage: " << program_name << " [filepath] | --repl" << std::endl;
+    std::cout << "  [filepath]   Execute a .peli script file." << std::endl;
+    std::cout << "  --repl         Enter interactive REPL mode." << std::endl;
+}
+
 int main(int argc, char* argv[]) {
-    if (find_option(argc, argv, "--repl")) {
+    if (argc < 2) {
+        printUsage(argv[0]);
+        return 1;
+    }
+
+    std::string argument = argv[1];
+
+    if (argument == "--repl") {
         runRepl();
+    } else if (argument.rfind("--", 0) != 0) { // Check if it's NOT a flag
+        runFile(argument);
     } else {
-        std::string input = "10 5 DUP * -";
-        std::cout << "Input string: \"" << input << "\"" << std::endl;
-        bool generate_ast = find_option(argc, argv, "--ppm_gen_ast_tree");
-        runSingle(input, generate_ast, argv[0]);
+        std::cerr << "Error: Unknown flag '" << argument << "'" << std::endl;
+        printUsage(argv[0]);
+        return 1;
     }
 
     return 0;
